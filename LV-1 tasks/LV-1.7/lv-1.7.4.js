1705ff486055de7aa9a535ea221440d7ee84dc3c
Dima <dyes099@mail.com>
2024-09-17 21:46:58 +0300

    reviewers

--- LV-1 tasks/LV-1.7/lv-1.7.4.js
+++ LV-1 tasks/LV-1.7/lv-1.7.4.js
@@ -8,7 +8,7 @@ const number = 12345;
 const fn = (number) => {
     let sum = 0;
     const numberStr = String(number);
-    for (let i = 0; i < numberStr.length; i ++) {
+    for (let i = 0; i < numberStr.length; i += 1) {
         sum += Number(numberStr[i]);
     }
     return sum;
--- LV-1 tasks/LV-1.8/lv-18.3.js
+++ LV-1 tasks/LV-1.8/lv-18.3.js
@@ -7,7 +7,7 @@ const arr = [1.456, 2.125, 3.32, 4.1, 5.34];
 
 const fn = (arr) => {
     const result = [];
-    for (let i = 0; i < arr.length; i ++) {
+    for (let i = 0; i < arr.length; i += 1) {
         result.push(arr[i].toFixed(1))
     }
     return result;
