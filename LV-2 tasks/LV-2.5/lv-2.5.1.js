cbdfe1b196f8ed2d8636125d680a815ba5a16042
Dima <dyes099@mail.com>
2024-09-19 14:18:31 +0300

    end 18.09.24

--- LV-2 tasks/LV-2.5/lv-2.5.1.js
+++ LV-2 tasks/LV-2.5/lv-2.5.1.js
@@ -5,3 +5,21 @@ const str = '023m0df0dfg0';
 
 const fn = (str) => str.split('').map((e, i)=> e == 0 ? i : '').filter(e => typeof e === 'number');
 console.log(fn(str));
+
+
+// const fn2 = (str) => {
+//     const result = [];
+//     result.push(str.split('').reduce((acc, e, i) => Number(e) === 0 ?  acc + i + ',' : acc, 0));
+//     return result;
+// };
+// console.log(fn2(str));
+
+const fn3 = (str) => {
+    return str.split('').reduce((acc, char, i) => {
+       if (Number(char) === 0) {
+        acc.push(i);   
+       };
+        return acc;
+    }, []);
+};
+console.log(fn3(str));
