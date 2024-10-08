2f64e37f2134a5ccc4f371e65558f38447ea6f0f
Dima <dyes099@mail.com>
2024-10-08 09:35:59 +0300

    fixed code branch 01.10.24

--- LV-3 tasks/LV-3.5/lv-3.5.4.js
+++ LV-3 tasks/LV-3.5/lv-3.5.4.js
@@ -2,5 +2,9 @@
 
 const arr = [20, 23, 25, 30, 33, 34, 43, 50, 15, 80, 90];
 
-const fn = (arr) => arr.filter(el => String(el).indexOf(3) > -1);
+// const fn = (arr) => arr.filter(el => String(el).indexOf(3) > -1);
+// console.log(fn(arr));
+
+const fn = (arr) => arr.filter(el => String(el).includes(3));
 console.log(fn(arr));
+
