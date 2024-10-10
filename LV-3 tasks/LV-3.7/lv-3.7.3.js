dcdcc4c221703464b2f947612d33b1f6556e0546
Dima <dyes099@mail.com>
2024-10-10 14:05:52 +0300

    finish branch 08.10.24

--- LV-3 tasks/LV-3.7/lv-3.7.3.js
+++ LV-3 tasks/LV-3.7/lv-3.7.3.js
@@ -5,9 +5,9 @@ const num2 = 40;
 
 const fn = (num1, num2) => {
     let result = [];
-    const whichOneIsShorter = num1 < num2 ? num1 : num2;
+    const shorter = num1 < num2 ? num1 : num2;
 
-    for(let i = 0; i < whichOneIsShorter; i += 1) {
+    for(let i = 0; i < shorter; i += 1) {
         (num1 % i === 0 && num2 % i === 0) && result.push(i);
     }
     return result;
